a47814552f349b87eabac2f136195e9baa13f65f
tach <tachterry@gmail.com>
2024-03-21 19:33:07 +0300

    feat: error messages for category

---
+++ apps/lesson/error_messages.py
@@ -0,0 +1,3 @@
+CATEGORY_NAME_LEN_ERROR_MESSAGE = "The length of the category name should be between 4 and 25 characters!"
+NON_UNIQUE_CATEGORY_NAME_ERROR_MESSAGE = "This category name is already in use."
+CATEGORY_DISCRIPTION_LEN_ERROR_MESSAGE = "The record category should not be longer than 1500 characters!"
