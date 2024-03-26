aff248da6f052bc5be1067d892b48ee36d95581c
tach <tachterry@gmail.com>
2024-03-26 19:17:45 +0300

    feat: Update login endpoint

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView
+)
+from apps.jwt_config.views import (
+    CustomTokenObtainPairView
 )
 
 
@@ -18,7 +20,7 @@ urlpatterns = [
     path("", ListUsersGenericView.as_view()),
     path("<int:user_id>/", UserDetailGenericView.as_view()),
     path("register/", UserRegistrationGenericView.as_view()),
-    path("auth/login/", TokenObtainPairView.as_view()),
+    path("auth/login/", CustomTokenObtainPairView.as_view()),
     path("auth/refresh-token/", TokenRefreshView.as_view()),
     path('users/<int:user_id>/pupils/', ListPupilsOfUserGenericView.as_view()),
     path('users/<int:user_id>/pupils/<int:pupil_id>/', PupilDetailGenericView.as_view(),),
